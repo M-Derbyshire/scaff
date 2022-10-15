2ca98a92666a9b2b174f830ba0120c41be091803
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2022-10-15 22:32:15 +0100

    setup uservariablemap package

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/M-Derbyshire/scaff/uservariablemap"
+)
+
+func main() {
+	userVariables := uservariablemap.GenerateVariableMap(os.Args[2:]) //Get the arguments after the app name, and the user's called command
+	fmt.Println(userVariables)
 }
---
+++ uservariablemap/uservariablemap.go
@@ -0,0 +1,20 @@
+package uservariablemap
+
+import "strings"
+
+// When given a slice of string arguments, this will convert any argument that contains an "=" symbol into a key/value pair in the returned map
+func GenerateVariableMap(args []string) map[string]string {
+
+	varMap := make(map[string]string)
+
+	for _, fullArg := range args {
+		splitArg := strings.SplitN(fullArg, "=", 2)
+		if len(splitArg) < 2 {
+			continue
+		}
+
+		varMap[splitArg[0]] = splitArg[1]
+	}
+
+	return varMap
+}
