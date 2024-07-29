8e388aa439c37195493a1ce27600f7f6fef43966
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2024-07-29 21:58:08 +0100

    comment correction

--- models/filescaffold.go
+++ models/filescaffold.go
@@ -5,7 +5,7 @@ import "path"
 // FileScaffold represents a file to be created
 type FileScaffold struct {
 	Name         string `json:"name"`         // The filename (including extension)
-	TemplatePath string `json:"templatePath"` // Path to the file's template (path relative to the command's template directory)
+	TemplatePath string `json:"templatePath"` // Path to the file's template (path relative to the template directory)
 }
 
 // GetFullTemplatePath returns the full path to the correct template (when given the path to the template directory)
